a879b2066faea75208b15225d4bab6c4e4d07e20
yotam-yakov <yotamyakov.w@gmail.com>
2024-12-17 16:36:33 +0200

    task 2 complete

--- ex4.c
+++ ex4.c
@@ -6,14 +6,15 @@ Assignment: ex4
 #include <stdio.h>
 #include <string.h>
 
+#define PYRAMID_LEVELS 5
+
 void task1RobotPaths();
 void task2HumanPyramid();
 void task3ParenthesisValidator();
 void task4QueensBattle();
 void task5CrosswordGenerator();
 
-int main()
-{
+int main() {
     int task = -1;
     do {
         printf("Choose an option:\n"
@@ -55,15 +56,6 @@ int main()
     } while (task != 6);
 }
 
-void task1RobotPaths() {
-    int row, column;
-    printf("Please enter the coordinates of the robot (column, row):");
-    scanf("%d %d", &row, &column);
-
-    printf("The total number of paths the robot can take to reach home is: %d\n", recursion(row, column));
-
-}
-
 int robotPaths(int r, int c) {
     if (r == 0 && c == 0) {
         return 1;
@@ -74,8 +66,45 @@ int robotPaths(int r, int c) {
     return robotPaths(r - 1, c) + robotPaths(r, c - 1);
 }
 
+void task1RobotPaths() {
+    int row, column;
+    printf("Please enter the coordinates of the robot (column, row):\n");
+    scanf("%d %d", &row, &column);
+
+    printf("The total number of paths the robot can take to reach home is: %d\n", robotPaths(row, column));
+
+}
+
+float pyramidWeight(float pyramid[][PYRAMID_LEVELS], int pyramidSize, int lvl, int pos) {
+    if(lvl < 0 || pos < 0 || pos > pyramidSize) {
+        return 0;
+    }
+    
+    float left = pyramidWeight(pyramid, pyramidSize, lvl - 1, pos - 1);
+    float right = pyramidWeight(pyramid, pyramidSize, lvl - 1, pos);
+
+    return (left / 2) + (right / 2) + pyramid[lvl][pos];
+}
+
 void task2HumanPyramid() {
-    // Todo
+    float pyramid[PYRAMID_LEVELS][PYRAMID_LEVELS] = {0};
+    float weights[PYRAMID_LEVELS][PYRAMID_LEVELS] = {0};
+    
+
+    printf("Please enter the weights of the cheerleaders: \n");
+    for(int i = 0; i < PYRAMID_LEVELS; i++) {
+        for(int j = 0; j <= i; j++)
+        scanf("%f", &pyramid[i][j]);
+    }
+
+    for(int i = 0; i < PYRAMID_LEVELS; i++) {
+        for(int j = 0; j <= i; j++) {
+            float posWeight = pyramidWeight(pyramid, PYRAMID_LEVELS, i, j);
+            weights[i][j] = posWeight;
+            printf("%.2f ", weights[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void task3ParenthesisValidator() {
